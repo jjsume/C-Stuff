a0bc41cd8f224608cea87f0db7ce334ad1fec1ad
Jere Sumell <34176980+jjsume@users.noreply.github.com>
2019-06-16 19:10:53 +0300

    Kertoma C:llä
    Laskee komentoriviltä syötetyn luvun kertoman.

---
+++ Kertoma.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+//Declaring function(s)
+int kertoma(int x);
+
+int kertoma(int x) {
+    if (x<=0) {
+    return 1;
+    }
+    return (x*kertoma(x-1));
+}
+//Main Method
+int main(int argc, char *argv[]) {
+    if (argc <=1) {
+        printf("Syötä parametrina luku, jonka kertoman tuloksen haluat selvittää!");
+    }
+    else
+    {
+        int tulos;
+        int input = atoi(argv[1]);
+        tulos = kertoma(input);
+        printf("%d",tulos);
+    }
+}
